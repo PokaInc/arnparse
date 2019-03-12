53f38b38b955f5c454b9233d84514c5b597998f8
spg <spgingras@poka.io>
2019-03-12 12:58:16 -0700

    Version 0.0.2

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 setup(
     name='arnparse',
     packages=['arnparse'],  # this must be the same as the name above
-    version='0.0.1',
+    version='0.0.2',
     description='Parse ARNs using Python',
     long_description=long_description,
     long_description_content_type='text/markdown',
